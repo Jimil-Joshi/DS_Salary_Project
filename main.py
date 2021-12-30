849d62af4616c8c12b8f4172668c68b45c17c724
Jimil-Joshi <87204649+Jimil-Joshi@users.noreply.github.com>
2021-12-30 21:53:03 +0530

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-import Glassdoor_Web_Scrapper as gs
-import pandas as pd 
-path =  "C:/Users/Jimil Joshi/Documents/DS_Salary_Project/chromedriver"
-
-df =  gs.get_jobs(keyword='data scientist',num_jobs=15,verbose = False,path = "C:/Users/Jimil Joshi/Documents/DS_Salary_Project/chromedriver.exe",slp_time=15)
